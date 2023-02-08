4f2cfc30ce30074c8ee7224c8b73775c75c90738
kubasn <sosinkuba@gmail.com>
2023-02-08 13:21:17 +0100

    tests performed on repositories components

---
+++ codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,55 @@
+import { render, screen } from "@testing-library/react";
+import RepositoriesListItem from "./RepositoriesListItem";
+import { MemoryRouter } from "react-router-dom";
+
+// jest.mock("../tree/FileIcon", () => {
+//   return () => {
+//     return "File Icon Component";
+//   };
+// });
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "JavaScript",
+    description: "A js library",
+    owner: { login: "facebook" },
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+
+  return { repository };
+}
+
+test("shows a link to the github repository webpage", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "JavaScript" });
+
+  const link = screen.getByRole("link", {
+    name: /github repository/i,
+  });
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a fileicon with the appropriate icon", async () => {
+  renderComponent();
+  const icon = await screen.findByRole("img", { name: "JavaScript" });
+
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
+});
